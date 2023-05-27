f6e6442dcad9e9f987a079113e8ef5cc162b74e1
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-05-27 17:00:40 +0600

    Update media--image.tsx

--- components/media--image.tsx
+++ components/media--image.tsx
@@ -1,4 +1,4 @@
-import Image, { ImageProps } from "next/legacy/image"
+import Image, { ImageProps } from "next/image"
 
 import { absoluteURL } from "lib/utils"
 import { MediaProps } from "components/media"
