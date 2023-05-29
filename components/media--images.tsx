9ea356d8e70fff7576f6dfad606f3fe8f1819773
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-05-29 14:08:32 +0600

    Update media--images.tsx

--- components/media--images.tsx
+++ components/media--images.tsx
@@ -1,44 +1,20 @@
-import Image, { ImageProps } from "next/legacy/image"
-
-import { absoluteURL } from "lib/utils"
+import { MediaImage } from './media--image';
 import { MediaProps } from "components/media"
 
-interface MediaImagesProps extends MediaProps, Partial<ImageProps> {}
-
-export function MediaImages({
-  media,
-  layout = "responsive",
-  objectFit,
-  width,
-  height,
-  priority,
-  ...props
-}: MediaImagesProps) {
-    const image = media?.field_media_images?.[0]
-
-  if (!image?.uri) {
-    return null
-  }
-
-  const sizeProps =
-    layout === "fill"
-      ? null
-      : {
-          width: width || image.resourceIdObjMeta.width,
-          height: height || image.resourceIdObjMeta.height,
-        }
-
-  return (
-    <div className="media__content image__wrapper" {...props}>
-      <Image
-        src={absoluteURL(image.uri.url)}
-        layout={layout}
-        objectFit={objectFit}
-        alt={image.resourceIdObjMeta.alt || "Image"}
-        title={image.resourceIdObjMeta.title}
-        priority={priority}
-        {...sizeProps}
-      />
-    </div>
-  )
+interface MediaImagesProps {
+  media: Array<MediaProps>;
 }
+
+export function MediaImages({ media }: MediaImagesProps) {
+    if (!media) {
+      return null;
+    }
+
+    return (
+      <div>
+        {media.map((item, index) => (
+          <MediaImage key={index} media={item} />
+        ))}
+      </div>
+    );
+  }
