a3ddfafa3e95d7694ffdcad38639eeb501e4b2fa
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-05-29 14:09:30 +0600

    Update paragraph--incentive_with_illustration.tsx

--- components/paragraph--incentive_with_illustration.tsx
+++ components/paragraph--incentive_with_illustration.tsx
@@ -14,14 +14,14 @@ export function ParagraphIncentiveWithIllustration({
             <Link href={paragraph.field_link.uri}>
                 {/* <img src={paragraph.field_image.uri} alt="" className="h-24 w-auto" /> */}
                 <MediaImage
-                            media={paragraph.field_media_image}
-                            height="256"
-                            width="256"
-                            // priority
-                            // layout="fill"
-                            // objectFit="cover"
-                            // className="h-full w-full object-cover object-center"
-                        />
+                    media={paragraph.field_media_image}
+                    height="256"
+                    width="256"
+                    // priority
+                    // layout="fill"
+                    // objectFit="cover"
+                    // className="h-full w-full object-cover object-center"
+                />
                 <h3 className="mt-6 text-sm font-medium text-gray-900">{paragraph.field_text_title}</h3>
                 <p className="mt-2 text-sm text-gray-500">{paragraph.field_text_tagline}</p>
             </Link>
