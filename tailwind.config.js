193bd340b6948c75f343f1850865c0f2acbec7b8
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-05-29 14:09:14 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
-const { fontFamily } = require('tailwindcss/defaultTheme');
+// const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
     content: [
