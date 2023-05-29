700a4420e878397a936d4da1b2a3271a8fd5937a
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-05-29 14:10:13 +0600

    Update paragraph.tsx

--- components/paragraph.tsx
+++ components/paragraph.tsx
@@ -2,11 +2,22 @@ import { DrupalParagraph } from 'next-drupal';
 import { ParagraphBlogSection } from 'components/paragraph--blog-section';
 import { ParagraphImageFullWidth } from './paragraph--image_full_width';
 import { ParagraphVideo } from './paragraph--video';
+import { ParagraphPromoBgImageLargeContent } from 'components/paragraph--promo_section_bg_image_large';
+import { ParagraphPromoBackgroundImage } from 'components/paragraph--promo_section_background_image';
+import { ParagraphPromoFullWidthTiles } from 'components/paragraph--promo_section_full_width_tiles';
+import { ParagraphProductFeaturesAlternating } from 'components/paragraph--product_features_alternating';
+import { ParagraphProductFeaturesSquareImages } from 'components/paragraph--product_features_square_images';
 
 const paragraphTypes = {
     'paragraph--blog_section': ParagraphBlogSection,
     'paragraph--image_full_width': ParagraphImageFullWidth,
     'paragraph--video': ParagraphVideo,
+    'paragraph--promo_section_bg_image_large': ParagraphPromoBgImageLargeContent,
+    'paragraph--promo_section_background_image': ParagraphPromoBackgroundImage,
+    'paragraph--promo_section_full_width_tiles': ParagraphPromoFullWidthTiles,
+    'paragraph--product_features_alternating': ParagraphProductFeaturesAlternating,
+    'paragraph--product_features_square_images': ParagraphProductFeaturesSquareImages,
+    // Add other paragraph types as needed
 };
 
 export interface ParagraphProps {
