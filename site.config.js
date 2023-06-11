b601280898eeca0ece4754b5c6ba20301c366fd0
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-06-11 15:16:58 +0600

    Add Russian language
    and commented Spanish

--- site.config.js
+++ site.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    name: 'Destination Jeti-Oguz',
-    slogan: 'The Destination Jeti-Oguz site built as a headless Drupal with Next.js',
+    name: 'Destination Jeti Oguz',
+    slogan: 'Explore the other side of Issyk-Kul lake.',
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
     drupalBaseUrl: process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
     defaultLocale: 'en',
     locales: {
         en: 'English',
-        es: 'Español',
+        ru: 'Русский',
+        // es: 'Español',
     },
 };
