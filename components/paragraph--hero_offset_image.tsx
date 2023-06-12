cc7817f24c2ea1dc19a9511f6fbeea3e9a60dc48
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-06-12 06:35:48 +0600

    Update top elements

--- components/paragraph--hero_offset_image.tsx
+++ components/paragraph--hero_offset_image.tsx
@@ -9,11 +9,58 @@ export interface ParagraphProps {
 export function ParagraphHeroOffsetImage({ paragraphType }: ParagraphProps) {
 
     return (
-        <div className="relative isolate overflow-hidden bg-gradient-to-b from-primary-100/20 pt-14">
-            <div
+        <div className="relative isolate overflow-hidden pt-14">
+            {/* <div
                 className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-primary-600/10 ring-1 ring-primary-50 sm:-mr-80 lg:-mr-96"
                 aria-hidden="true"
-            />
+            /> */}
+            <svg
+                    className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
+                    aria-hidden="true"
+                >
+                    <defs>
+                        <pattern
+                            id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
+                            width={200}
+                            height={200}
+                            x="50%"
+                            y={-1}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <path d="M.5 200V.5H200" fill="none" />
+                        </pattern>
+                    </defs>
+                    <svg
+                        x="50%"
+                        y={-1}
+                        className="overflow-visible fill-gray-50"
+                    >
+                        <path
+                            d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+                            strokeWidth={0}
+                        />
+                    </svg>
+                    <rect
+                        width="100%"
+                        height="100%"
+                        strokeWidth={0}
+                        fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)"
+                    />
+            </svg>
+            <div
+                    className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
+                    aria-hidden="true"
+                >
+                    <div
+                        className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#fed7aa] to-[#c2410c] opacity-30"
+                        style={{
+                            clipPath:
+                                'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
+                        }}
+                    />
+                </div>
+
+
             <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
                     <h1 className="max-w-2xl text-4xl font-heading font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
