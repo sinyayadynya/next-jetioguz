f48ea12e21173f5856bdc5964fc4c8780cad064c
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-06-13 04:33:39 +0600

    Update logo

--- components/header.tsx
+++ components/header.tsx
@@ -158,13 +158,13 @@ export function Header() {
     return (
         <header className="absolute inset-x-0 top-0 z-50 bg-white bg-opacity-90 backdrop-blur-sm backdrop-filter font-sans">
             <nav
-                className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4 lg:px-8"
+                className="mx-auto flex max-w-7xl items-center justify-between px-6 py-2 lg:px-8"
                 aria-label="Global"
             >
                 <div className="flex lg:flex-1">
                     <Link href="/" passHref className="-m-1.5 p-1.5">
                         <span className="sr-only">{siteConfig.name}</span>
-                        <Logo className="h-9 w-auto text-primary-600" />
+                        <Logo className="h-12 w-auto text-primary-600" />
                     </Link>
                 </div>
                 <div className="flex lg:hidden">
