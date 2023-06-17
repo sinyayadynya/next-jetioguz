9986a97c9c62d07ceef72d1905b083c10416586d
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-06-17 19:25:11 +0600

    Update webform--accommodation.tsx

--- components/webform--accommodation.tsx
+++ components/webform--accommodation.tsx
@@ -95,27 +95,6 @@ export default function AccommodationForm() {
                         </div>
                     </div>
 
-                    {/* Project description */}
-                    <div className="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
-                        <div>
-                            <label
-                                htmlFor="project-description"
-                                className="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5"
-                            >
-                                Description
-                            </label>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <textarea
-                                id="project-description"
-                                name="project-description"
-                                rows={3}
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-                                defaultValue={''}
-                            />
-                        </div>
-                    </div>
-
                     {/* Check in */}
                     <div className="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:items-center sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
                         <div>
