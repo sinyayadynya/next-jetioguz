b9aecc3e24085de49ca31551c521f83e54a248af
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-06-17 19:09:00 +0600

    Update node--place--teaser.tsx

--- components/node--place--teaser.tsx
+++ components/node--place--teaser.tsx
@@ -23,13 +23,30 @@ export function NodePlaceTeaser({ node, ...props }: NodePlaceTeaserProps) {
                 <div className="mt-4 flex justify-between">
                     <div>
                         <h3 className="font-semibold text-gray-900">
-                        {node.path.alias ? (
+                            <Link href={node.path.alias} passHref>
+                                <span
+                                    aria-hidden="true"
+                                    className="absolute inset-0"
+                                />
+                                {node.title}
+                            </Link>
+
+
+                            {node.path.alias ? (
                                 <Link href={node.path.alias} passHref>
-                                    {/* ... */}
+                                    <span
+                                        aria-hidden="true"
+                                        className="absolute inset-0"
+                                    />
+                                    {node.title}
                                 </Link>
                                 ) : (
                                 <div>
-                                    No place found.
+                                    <span
+                                        aria-hidden="true"
+                                        className="absolute inset-0"
+                                    />
+                                    {node.title}
                                 </div>
                             )}
                         </h3>
