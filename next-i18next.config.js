2bea2ad8a27e071575351ab378df556156da80c6
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-06-19 22:17:18 +0600

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     i18n: {
         defaultLocale: config.defaultLocale,
         locales: Object.keys(config.locales),
+        localePath: path.resolve('./public/locales'),
     },
     ns: ['common'],
 };
