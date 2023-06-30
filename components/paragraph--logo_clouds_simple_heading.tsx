be8cd77b41be5f6bc846aec135219c811b1d9b55
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-06-30 20:24:24 +0600

    Update logo clouds
    https://height.app/0Wn5_wGZ1h/T-78

--- components/paragraph--logo_clouds_simple_heading.tsx
+++ components/paragraph--logo_clouds_simple_heading.tsx
@@ -1,4 +1,5 @@
 import { DrupalParagraph } from 'next-drupal';
+import Image from 'next/image';
 import Link from 'next/link';
 import { MediaImage } from './media--image';
 
@@ -14,41 +15,51 @@ export function ParagraphLogoCloudSimpleHeading({ paragraphType }: ParagraphProp
                     {paragraphType.field_heading}
                 </h2>
                 <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-                    <img
-                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-                        alt="Transistor"
-                        width={158}
-                        height={48}
-                    />
-                    <img
-                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-                        alt="Reform"
-                        width={158}
-                        height={48}
-                    />
-                    <img
-                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-                        alt="Tuple"
-                        width={158}
-                        height={48}
-                    />
-                    <img
-                        className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-                        alt="SavvyCal"
-                        width={158}
-                        height={48}
-                    />
-                    <img
-                        className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-                        alt="Statamic"
-                        width={158}
-                        height={48}
-                    />
+                    <Link href="https://nomadsland.travel" target="_blank">
+                        <Image
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="/logos/158x48/nomadsland-logo-color.svg"
+                            alt="Nomad's Land"
+                            width={158}
+                            height={48}
+                        />
+                    </Link>
+                    <Link href="https://trekkistan.com" target="_blank">
+                        <Image
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="/logos/158x48/trekkistan-logo-color.svg"
+                            alt="Trekkistan"
+                            width={158}
+                            height={48}
+                        />
+                    </Link>
+                    <Link href="https://cluster.kg" target="_blank">
+                        <Image
+                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            src="/logos/158x48/cluster-logo-color.svg"
+                            alt="Mountain Cluster"
+                            width={158}
+                            height={48}
+                        />
+                    </Link>
+                    <Link href="https://teskei-geopark.com" target="_blank">
+                        <Image
+                            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                            src="/logos/158x48/teskei-logo-color.svg"
+                            alt="Teskei Geopark"
+                            width={158}
+                            height={48}
+                        />
+                    </Link>
+                    <Link href="https://snowreport.kg" target="_blank">
+                        <Image
+                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src="/logos/158x48/snowreport-logo-color.svg"
+                            alt="SnowReport"
+                            width={158}
+                            height={48}
+                        />
+                    </Link>
                 </div>
             </div>
         </div>
