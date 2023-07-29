321636657741bef733544eeb0c10f916ac52569c
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-29 06:28:47 +0600

    Update map

--- components/CustomControl.tsx
+++ components/CustomControl.tsx
@@ -1,86 +1,116 @@
 // ./components/CustomControl.tsx
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react'; // Import useRef
 import { useMap } from 'react-leaflet';
-import L from 'leaflet';
+import L, { Control, Layer, LayerGroup } from 'leaflet'; // Import types from 'leaflet'
 import 'leaflet.markercluster';
 
 declare module 'leaflet' {
-  export function markerClusterGroup(options?: any): any;
+    export function markerClusterGroup(options?: any): any;
 }
 
-export default function CustomControl({ layers }) {
-  const map = useMap();
-  const [layerGroups, setLayerGroups] = useState({});
+export default function CustomControl({ layers, language }) {
+    const map = useMap();
+    const prevLayersRef = useRef<Record<string, LayerGroup>>({}); // Provide explicit type
+    const customControlRef = useRef<Control>(); // Provide explicit type
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const newLayerGroups = {};
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // Fade out old layers
+            if (prevLayersRef.current) {
+                for (const layerGroup of Object.values(prevLayersRef.current)) {
+                    const group = layerGroup as any; // Use 'any' type
+                    group.eachLayer((layer: any) => {
+                        if (layer instanceof L.Marker) {
+                            layer.setOpacity(0); // Set opacity to 0
+                        }
+                    });
+                }
+            }
 
-      layers.forEach(layer => {
-        const markerClusterGroup = L.markerClusterGroup();
-        newLayerGroups[layer.name] = markerClusterGroup;
+            // Remove old control
+            if (customControlRef.current) {
+                map.removeControl(customControlRef.current);
+            }
 
-        fetch(`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${layer.endpoint}`)
-          .then(response => response.json())
-          .then(data => {
-            data.features.forEach(item => {
-              const customIcon = new L.Icon({
-                iconUrl: layer.iconUrl,
-                iconRetinaUrl: layer.iconUrl.replace('.png', '-2x.png'),
-                shadowUrl: '/images/marker-shadow.png',
-                iconSize: [25, 41],
-                iconAnchor: [12, 41],
-                popupAnchor: [1, -34],
-                shadowSize: [41, 41]
-              });
+            const newLayerGroups = {};
 
-              const marker = L.marker(item.geometry.coordinates.reverse(), { icon: customIcon });
-              marker.bindPopup(`${item.properties.name} - ${item.properties.description}`);
-              markerClusterGroup.addLayer(marker);
+            layers.forEach(layer => {
+                const markerClusterGroup = L.markerClusterGroup();
+                newLayerGroups[layer.name] = markerClusterGroup;
+
+                fetch(`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/${language}${layer.endpoint}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        data.features.forEach(item => {
+                            const customIcon = new L.Icon({
+                                iconUrl: layer.iconUrl,
+                                iconRetinaUrl: layer.iconUrl.replace('.png', '-2x.png'),
+                                shadowUrl: '/images/marker-shadow.png',
+                                iconSize: [25, 41],
+                                iconAnchor: [12, 41],
+                                popupAnchor: [1, -34],
+                                shadowSize: [41, 41]
+                            });
+
+                            const marker = L.marker(item.geometry.coordinates.reverse(), { icon: customIcon });
+                            marker.bindPopup(`${item.properties.name} - ${item.properties.description}`);
+                            markerClusterGroup.addLayer(marker);
+                        });
+
+                        map.addLayer(markerClusterGroup);
+                    });
             });
 
-            map.addLayer(markerClusterGroup);
-          });
-      });
+            // Fade in new layers
+            for (const layerGroup of Object.values(newLayerGroups)) {
+                const group = layerGroup as any; // Use 'any' type
+                group.eachLayer((layer: any) => {
+                    if (layer instanceof L.Marker) {
+                        layer.setOpacity(1); // Set opacity to 1
+                    }
+                });
+            }
 
-      setLayerGroups(newLayerGroups);
+            prevLayersRef.current = newLayerGroups; // Update the reference to the current layers
 
-      const customControl = L.Control.extend({
-        options: {
-          position: 'topright'
-        },
-        onAdd: function () {
-          const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom z-50');
-          container.className = "border-gray-300 relative flex flex-col cursor-pointer rounded-lg border bg-white px-3 py-1 shadow-sm";
-          const fieldset = L.DomUtil.create('fieldset', '', container);
-          const legend = L.DomUtil.create('legend', 'sr-only', fieldset);
-          legend.innerHTML = "Overlays";
-          const div = L.DomUtil.create('div', 'divide-gray-200 divide-y', fieldset);
-          layers.forEach(layer => {
-            const label = L.DomUtil.create('label', 'relative flex items-start', div);
-            const div2 = L.DomUtil.create('div', 'flex h-8 items-center', label);
-            const input = L.DomUtil.create('input', '', div2);
-            input.type = 'checkbox';
-            input.checked = true;
-            input.className = `h-4 w-4 rounded border-gray-300 ${layer.color}`;
-            L.DomEvent.on(input, 'click', function () {
-              if (this.checked) {
-                map.addLayer(newLayerGroups[layer.name]);
-              } else {
-                map.removeLayer(newLayerGroups[layer.name]);
-              }
+            const customControl = L.Control.extend({
+                options: {
+                    position: 'topright'
+                },
+                onAdd: function () {
+                    const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom z-50');
+                    container.className = "border-gray-300 relative flex flex-col cursor-pointer rounded-lg border bg-white px-3 py-1 shadow-sm";
+                    const fieldset = L.DomUtil.create('fieldset', '', container);
+                    const legend = L.DomUtil.create('legend', 'sr-only', fieldset);
+                    legend.innerHTML = "Overlays";
+                    const div = L.DomUtil.create('div', 'divide-gray-200 divide-y', fieldset);
+                    layers.forEach(layer => {
+                        const label = L.DomUtil.create('label', 'relative flex items-start', div);
+                        const div2 = L.DomUtil.create('div', 'flex h-8 items-center', label);
+                        const input = L.DomUtil.create('input', '', div2);
+                        input.type = 'checkbox';
+                        input.checked = true;
+                        input.className = `h-4 w-4 rounded border-gray-300 ${layer.color}`;
+                        L.DomEvent.on(input, 'click', function () {
+                            if (this.checked) {
+                                map.addLayer(newLayerGroups[layer.name]);
+                            } else {
+                                map.removeLayer(newLayerGroups[layer.name]);
+                            }
+                        });
+                        const div3 = L.DomUtil.create('div', 'ml-3 text-sm leading-8', label);
+                        const label2 = L.DomUtil.create('label', 'font-medium text-gray-900', div3);
+                        label2.innerHTML = ` ${layer.name}`;
+                    });
+                    return container;
+                }
             });
-            const div3 = L.DomUtil.create('div', 'ml-3 text-sm leading-8', label);
-            const label2 = L.DomUtil.create('label', 'font-medium text-gray-900', div3);
-            label2.innerHTML = ` ${layer.name}`;
-          });
-          return container;
+
+            const newCustomControl = new customControl();
+            map.addControl(newCustomControl);
+            customControlRef.current = newCustomControl; // Update the reference to the current control
         }
-      });
+    }, [map, layers, language]);
 
-      map.addControl(new customControl());
-    }
-  }, [map, layers]);
-
-  return null;
+    return null;
 }
--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,13 +1,14 @@
 // ./components/MapComponent.tsx
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import { useTranslation } from 'react-i18next';
 
 const CustomControl = dynamic(() => import('components/CustomControl'), { ssr: false });
-
 
 // Dynamically import MapContainer, TileLayer, Marker, Popup
 const MapContainer = dynamic(
@@ -20,64 +21,44 @@ const TileLayer = dynamic(
   { ssr: false }
 );
 
-// Import LayersControl and its sub-components
-const LayersControl = dynamic(
-    () => import('react-leaflet').then((mod) => mod.LayersControl),
-    { ssr: false }
-  );
-
-  const BaseLayer = dynamic(
-    () => import('react-leaflet').then((mod) => mod.LayersControl.BaseLayer),
-    { ssr: false }
-  );
-
-  const Overlay = dynamic(
-    () => import('react-leaflet').then((mod) => mod.LayersControl.Overlay),
-    { ssr: false }
-  );
-
-  const AccommodationLayer = dynamic(() => import('components/MarkerCluster'), { ssr: false });
-  const NatureSiteLayer = dynamic(() => import('components/MarkerCluster'), { ssr: false });
-  const CultureSiteLayer = dynamic(() => import('components/MarkerCluster'), { ssr: false });
-  const WellnessLayer = dynamic(() => import('components/MarkerCluster'), { ssr: false });
-  const VillageLayer = dynamic(() => import('components/MarkerCluster'), { ssr: false });
-
 export default function MapComponent() {
+    const { t } = useTranslation();
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const L = require('leaflet');
+    const { locale } = useRouter(); // Get the current locale
 
-      delete L.Icon.Default.prototype._getIconUrl;
+    const layers = [
+        { name: t('accommodations'), endpoint: "/api/jetioguz/accommodation/map", iconUrl: "/images/accommodation-icon.png", color: "text-sky-600 focus:ring-sky-600" },
+        { name: t('nature-sites'), endpoint: "/api/jetioguz/nature/map", iconUrl: "/images/nature-site-icon.png", color: "text-green-600 focus:ring-green-600" },
+        { name: t('culture-sites'), endpoint: "/api/jetioguz/culture/map", iconUrl: "/images/culture-site-icon.png", color: "text-primary-600 focus:ring-primary-600" },
+        { name: t('wellness'), endpoint: "/api/jetioguz/wellness/map", iconUrl: "/images/wellness-icon.png", color: "text-blue-600 focus:ring-blue-600" },
+        { name: t('villages'), endpoint: "/api/jetioguz/village/map", iconUrl: "/images/village-icon.png", color: "text-red-600 focus:ring-red-600" },
+    ];
 
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: '/images/marker-icon-2x.png', // replace with your image path
-        iconUrl: '/images/marker-icon.png', // replace with your image path
-        shadowUrl: '/images/marker-shadow.png', // replace with your image path
-      });
-    }
-  }, []);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const L = require('leaflet');
 
+            delete L.Icon.Default.prototype._getIconUrl;
 
-  const layers = [
-    { name: 'Accommodation', endpoint: "/api/jetioguz/accommodation/map", iconUrl: "/images/accommodation-icon.png", color: "text-sky-600 focus:ring-sky-600" },
-    { name: 'Nature Sites', endpoint: "/api/jetioguz/nature/map", iconUrl: "/images/nature-site-icon.png", color: "text-green-600 focus:ring-green-600" },
-    { name: 'Culture Sites', endpoint: "/en/api/jetioguz/culture/map", iconUrl: "/images/culture-site-icon.png", color: "text-primary-600 focus:ring-primary-600" },
-    { name: 'Wellness', endpoint: "/en/api/jetioguz/wellness/map", iconUrl: "/images/wellness-icon.png", color: "text-blue-600 focus:ring-blue-600" },
-    { name: 'Villages', endpoint: "/en/api/jetioguz/village/map", iconUrl: "/images/village-icon.png", color: "text-red-600 focus:ring-red-600" },
-  ];
+            L.Icon.Default.mergeOptions({
+                iconRetinaUrl: '/images/marker-icon-2x.png', // replace with your image path
+                iconUrl: '/images/marker-icon.png', // replace with your image path
+                shadowUrl: '/images/marker-shadow.png', // replace with your image path
+            });
+        }
+    }, []);
 
-  return (
-    <div style={{ height: 'calc(100vh - 64px)', width: '100%' }} className='mt-16'>
-        {typeof window !== 'undefined' && (
-          <MapContainer center={[42.35, 78.06]} zoom={11} style={{ height: '100%', width: '100%' }}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <CustomControl layers={layers} />
-          </MapContainer>
-        )}
-      </div>
-  )
+    return (
+        <div style={{ height: 'calc(100vh - 64px)', width: '100%' }} className='mt-16'>
+            {typeof window !== 'undefined' && (
+                <MapContainer center={[42.35, 78.06]} zoom={11} style={{ height: '100%', width: '100%' }}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <CustomControl layers={layers} language={locale} /> {/* Pass the current locale */}
+                </MapContainer>
+            )}
+        </div>
+    )
 }
