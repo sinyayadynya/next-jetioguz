749515d55477283b187515e5d22598ebd5844245
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-07-30 02:55:21 +0600

    Fix XML sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    siteUrl: 'https://jetioguz.travel', // replace with your site's URL
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+    // sitemapSize: 5000,
+    // exclude: ['/path/to/exclude'],
+    // priority: 0.7,
+    // changefreq: 'daily',
+    // robotsTxtOptions: {
+    //   additionalSitemaps: [
+    //     'https://example.com/my-custom-sitemap.xml',
+    //   ],
+    // },
+};
--- next-sitemap.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    siteUrl: 'https://jetioguz.travel', // replace with your site's URL
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-};
