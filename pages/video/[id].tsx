4a13a164aab166dd69c97cb1c61a6d9f1ad9b844
Sinyaya Dynya <sinyayadynya@gmail.com>
2023-08-02 22:55:45 +0600

    Update [id].tsx

--- pages/video/[id].tsx
+++ pages/video/[id].tsx
@@ -14,11 +14,11 @@ import {
     Squares2X2Icon as Squares2X2IconMini,
 } from '@heroicons/react/20/solid';
 
-const tabs = [
-    { name: 'Recently Viewed', href: '#', current: true },
-    { name: 'Recently Added', href: '#', current: false },
-    { name: 'Favorited', href: '#', current: false },
-];
+// const tabs = [
+//     { name: 'Recently Viewed', href: '#', current: true },
+//     { name: 'Recently Added', href: '#', current: false },
+//     { name: 'Favorited', href: '#', current: false },
+// ];
 
 // function classNames(...classes) {
 //     return classes.filter(Boolean).join(' ')
@@ -209,79 +209,7 @@ export default function Channel({ title, videos }) {
                             </div>
                         </div>
 
-                        {/* Tabs */}
-                        <div className="mt-3 sm:mt-2">
-                            <div className="sm:hidden">
-                                <label htmlFor="tabs" className="sr-only">
-                                    Select a tab
-                                </label>
-                                {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-                                <select
-                                    id="tabs"
-                                    name="tabs"
-                                    className="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:border-primary-500 focus:ring-2 focus:ring-inset focus:ring-primary-600"
-                                    defaultValue="Recently Viewed"
-                                >
-                                    <option>Recently Viewed</option>
-                                    <option>Recently Added</option>
-                                    <option>Favorited</option>
-                                </select>
-                            </div>
-                            <div className="hidden sm:block">
-                                <div className="flex items-center border-b border-gray-200">
-                                    <nav
-                                        className="-mb-px flex flex-1 space-x-6 xl:space-x-8"
-                                        aria-label="Tabs"
-                                    >
-                                        {tabs.map((tab) => (
-                                            <a
-                                                key={tab.name}
-                                                href={tab.href}
-                                                aria-current={
-                                                    tab.current
-                                                        ? 'page'
-                                                        : undefined
-                                                }
-                                                className={classNames(
-                                                    tab.current
-                                                        ? 'border-primary-500 text-primary-600'
-                                                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                                                    'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
-                                                )}
-                                            >
-                                                {tab.name}
-                                            </a>
-                                        ))}
-                                    </nav>
-                                    <div className="ml-6 hidden items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
-                                        <button
-                                            type="button"
-                                            className="rounded-md p-1.5 text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
-                                        >
-                                            <Bars4Icon
-                                                className="h-5 w-5"
-                                                aria-hidden="true"
-                                            />
-                                            <span className="sr-only">
-                                                Use list view
-                                            </span>
-                                        </button>
-                                        <button
-                                            type="button"
-                                            className="ml-0.5 rounded-md bg-white p-1.5 text-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
-                                        >
-                                            <Squares2X2IconMini
-                                                className="h-5 w-5"
-                                                aria-hidden="true"
-                                            />
-                                            <span className="sr-only">
-                                                Use grid view
-                                            </span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
 
                         <section
                             className="mt-8 pb-16"
